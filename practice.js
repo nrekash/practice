6789f7d4e7642ee31aeb9df1d27979e6bf5b7fdb
nrekash <105354812+nrekash@users.noreply.github.com>
2024-06-26 17:55:11 +0530

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,28 @@
+// console.log("hello");
+
+// let a=BigInt(1235635454636355366);
+// let name=null;
+// console.log(name);
+// console.log(a);
+
+// let person={
+//     name:'rekash',
+//     std:'mca',
+//     age:25
+// };
+// console.log(person);
+
+// let a=3-1;
+// let b=5;
+// let c=a+b;
+// let d=b/a;
+// console.log(a,b,c,d);
+
+let a=1;
+a++;
+b=2;
+
+console.log(a+b);
+if(a==b){
+    console.log("true");
+}
